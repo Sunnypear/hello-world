98e84c9c281a23fb5fa2ebe267e232c926b886db
yanzhilong <1782935632@qq.com>
2018-12-23 00:47:10 +0800

    hello-world.c

--- BasicPrime.c
+++ BasicPrime.c
@@ -1,5 +1,33 @@
-//ç´ æ•°æ˜¯åªæœ‰1å’Œæœ¬èº«æ˜¯å› å­çš„æ•°
-//2018/11/14 æ›´æ–°
-int main() {
+//ËØÊıÊÇÖ»ÓĞ1ºÍ±¾ÉíÊÇÒò×ÓµÄÊı
+//2018/11/14 ¸üĞÂ
+/*int main() {
     //to do sth
-}
+}*/
+#include<stdio.h>
+int main()
+{
+	int num,i,b=2,lab=0;
+	scanf("%d",&num);
+	for(i=0;i<num-1;i++)
+	{   
+	    lab=0;
+	    int a=num%b;
+		b++;
+		if(b==num)
+		{   
+			break;
+			}	
+		if(a==0)
+		{   
+		    lab=1;
+			break;
+		}
+	}
+	if(lab==1&&num!=2)
+	{
+		printf("²»ÊÇËØÊı"); 
+	}
+	else
+	printf("ÊÇËØÊı");
+	return 0;
+ } 
--- RollNum.c
+++ RollNum.c
@@ -1,5 +1,29 @@
-//å›æ–‡æ•°ï¼šæ­£è¯»ä¸åè¯»ç›¸ç­‰çš„æ•°
-//2018/11/14 æ›´æ–°
-int main() {
+//»ØÎÄÊı£ºÕı¶ÁÓë·´¶ÁÏàµÈµÄÊı
+//2018/11/14 ¸üĞÂ
+/*int main() {
     //to do sth
-}
+}*/
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char ch[1010];
+    scanf("%s",ch);
+    int l=strlen(ch),i,j=0,lab=0;
+    for(i=0;i<l;i++)
+    {    lab=0;
+    	if(ch[j]==ch[l-j-1])
+    	{
+    	   lab=1;
+		}
+		j++;
+	}
+	if(lab==0)
+	{
+		printf("no");
+	}
+	else
+	{printf("yes");
+	}
+	return 0;
+ } 
